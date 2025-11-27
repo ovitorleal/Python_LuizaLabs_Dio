934c38a75a6115436d23242219780819f7a4e591
ovitorleal <vitorleal90@outlook.com>
2025-11-27 09:10:36 -0300

    Estudos 27/11 💻

---
+++ SANTANDER/Bancos_Dados/MongoDB.py
@@ -0,0 +1,26 @@
+'''NOSQL = 
+NOT only SQL.
+Nao segue modelo de tabeles e relacionamentos.
+Projetados para lidar com altos volumes de dados e alta escalabilidade.
+Alta flexibilidade no modelo de dados.
+Amplamente utilizados em cenarios onde a consistencia dos dados nao e critica.
+
+Diferenças entre SQL e NoSQL:
+-> SQL: Modelo de dados fixo / Escalabilidade vertical (hardware) / Transações ACID 100% / Linguagem de consulta estruturada (SQL)
+-> NoSQL: Modelo de dados flexivel / Escalabilidade horizontal (adicionar mais servidores) / Transações ACID parciais ou ausentes /cada SGBD tem sua propria linguagem
+
+VANTAGENS NoSQL:
+=> Flexibilidade no modelo de dados
+=> Alta escalabilidade
+=> Melhor desempenho em cenario de consulta intensiva
+=> Tolerancia a falhas
+
+DESVANTAGENS NoSQL:
+=> Menor consistencia de dados imediata
+=> Menor suporte a consultas complexas (Dependendo do SGBD)
+
+
+
+
+
+'''
